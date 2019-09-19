5da63b87ae8e67cf241015042bac0cbe0dc7c90c
WuShenghui <770577856@qq.com>
2019-09-19 16:10:09 +0800

    gradient decent

--- lib/optimize/gradient.m
+++ lib/optimize/gradient.m
@@ -2,9 +2,17 @@ function G = gradient(trainSpikeY, lambdaYpredict, lambdaZ, trainSpikeX, theta)
     [Nx, H] = size(trainSpikeX);
     [Nz, ~] = size(lambdaZ);
 
-    % not very clear about how to calculate the gradient
-    % G.w = permute(repmat((trainSpikeY - lambdaYpredict) * lambdaZ', [Nx,1,H]), [1, 3, 2]); 
-    % G.w0 = repmat(sum(trainSpikeY - lambdaYpredict), [Nz, 1]);
-    % G.theta = theta .* ((trainSpikeY - lambdaYpredict) .* lambdaZ .* (1 - lambdaZ)) * 
-    % G.theta0 = 
+    G.theta = (trainSpikeY - lambdaYpredict) * lambdaZ';
+
+    G.theta0 = sum(trainSpikeY - lambdaYpredict);
+
+    GalmbdZ2w = theta' .* ((trainSpikeY - lambdaYpredict) .* lambdaZ .* (1 - lambdaZ));
+    for j = 1:Nz
+        for i = 1:Nx
+            convRes = conv(GalmbdZ2w(j, :), flip(trainSpikeX(i, :)));
+            G.w(i, :, j) = convRes(1:H);
+        end
+    end
+
+    G.w0 = theta .* sum((trainSpikeY - lambdaYpredict) .* lambdaZ .* (1 - lambdaZ), 2)';
 end
--- main.m
+++ main.m
@@ -13,8 +13,8 @@ trainSpikeY = trainSet{1,4}; % get train output(GT) of Sinusoidal
 [Nx, H] = size(trainSet{1,1});
 Nz = 15;
 err = 1;
-tol = 0.001;
-v = 0.01;
+threshold = 1e-3;
+v = 1e-5;
 
 %% GLM models
 % need todo
@@ -24,10 +24,10 @@ v = 0.01;
 
 %% staged point process
 % initialize the params
-w = rand(Nx, H, Nz);
-w0 = rand(1, Nz) * 4 - 2;
-theta = rand(1, Nz) * 4 - 2;
-theta0 = rand() * 4 - 2;
+w = rand(Nx, H, Nz) - 0.5;
+w0 = rand(1, Nz) - 0.5;
+theta = rand(1, Nz) - 0.5;
+theta0 = 0; % rand();
 
 % use gradient ascent to maxmium the L
 
@@ -37,24 +37,33 @@ theta0 = rand() * 4 - 2;
 
 L = logLikelyhood(trainSpikeY, lambdaYpredict);
 
+history = L;
 iteration = 0;
-while(err > tol)
+while(err > threshold)
     G = gradient(trainSpikeY, lambdaYpredict, lambdaZ, trainSpikeX, theta);  % todo: complete the function
     
     % update params
-    w = w - v * G.w;
-    w0 = w0 - v * G.w0;
-    theta = theta - v * G.theta;
-    theta0 = theta0 - v * G.theta0;
-    
+    w = w + v * G.w;
+    w0 = w0 + v * G.w0;
+    theta = theta + v * G.theta;
+    theta0 = theta0 + v * G.theta0;
+
     % calculat with new params
     Lpre = L;
     [lambdaYpredict, spikeYpredict, lambdaZ] = model(trainSpikeX, w, w0, theta, theta0);
     L = logLikelyhood(trainSpikeY, lambdaYpredict);
     err = abs(L - Lpre);
     
+    history = [history, L];
+    figure(2)
+    plot(history)
+    
     iteration = iteration + 1;
 end
+
+plotData(trainSpikeY, lambdaYpredict, spikeYpredict)
+
+
 
 %% Assessing Goodness-of-fit
 % todo: calculate DBR with spikeYpredict
